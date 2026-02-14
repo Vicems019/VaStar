cc1f0efc9246f165492400afcdc3f660ead88114
Vicems019 <vicente.marin013@gmail.com>
2026-02-14 04:47:51 +0100

    0.2 commit - new screen + cleaning code + new features

--- VaStar.py
+++ VaStar.py
@@ -22,7 +22,8 @@ size_text = ""
 size_input = pygame.Rect((WIDTH-260)//2, HEIGHT//2, 140, 40)
 size_button = pygame.Rect((WIDTH+60)//2, HEIGHT//2, 100, 40)
 active = False
-
+current_screen = "menu"
+dim = 0
 
 def set_dimensiones(value):
     mensaje = (
@@ -42,7 +43,25 @@ def set_dimensiones(value):
 
     screen.blit(message_surface, (x, 280))
         
-        
+
+def dibujar_mapa(dim):
+    mapa = 600
+    tam_celdas = mapa // dim
+
+    coord_x = 60
+    coord_y = 100
+
+    for row in range(dim):
+        for col in range(dim):
+
+            rect = pygame.Rect(
+                coord_x + col * tam_celdas,
+                coord_y + row * tam_celdas,
+                tam_celdas,
+                tam_celdas
+            )
+
+            pygame.draw.rect(screen, (0, 0, 0), rect, 1)  # borde
 
 while running:
     clock.tick(60)
@@ -52,57 +71,65 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        # Detectar click del mouse
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if size_input.collidepoint(event.pos):
-                active = True
-            else:
-                active = False
+        # Eventos del menu
+        if current_screen == "menu":
 
-            if size_button.collidepoint(event.pos):
-                print("Botón pulsado. Valor:", size_text)
-                try:
-                    if int(size_text) > 0:
-                        #TODO new panel with the map
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if size_input.collidepoint(event.pos):
+                    active = True
+                else:
+                    active = False
+
+                if size_button.collidepoint(event.pos):
+                    try:
+                        if int(size_text) > 0:
+                            current_screen = "game"
+                            dim = int(size_text)
+                    except:
                         pass
 
-                except:
-                    raise TypeError("Can't parse into int")
-                
-
-        # Detectar escritura
-        if event.type == pygame.KEYDOWN and active:
-            if event.key == pygame.K_BACKSPACE:
-                size_text = size_text[:-1]
-            elif event.unicode.isdigit():
-                if len(size_text) < 4:
-                    size_text += event.unicode
+            if event.type == pygame.KEYDOWN and active:
+                if event.key == pygame.K_BACKSPACE:
+                    size_text = size_text[:-1]
+                elif event.unicode.isdigit():
+                    if len(size_text) < 4:
+                        size_text += event.unicode
+        elif current_screen == "game":
+            pass
 
     screen.fill((200, 200, 200))
 
-    # PANEL PRINCIPAL
-    text_main = font_title.render("Bienvenido a VaStar*", True, (0, 0, 0))
-    screen.blit(text_main, (130, 100))
+    # Diseño del menu
+    if current_screen == "menu":
 
-    # Cambiar color si está activo
-    input_color = (120, 120, 120) if active else (90, 90, 90)
+        text_main = font_title.render("Bienvenido a VaStar*", True, (0, 0, 0))
+        screen.blit(text_main, (130, 100))
 
-    # Campo input
-    pygame.draw.rect(screen, input_color, size_input, border_radius=10)
-    surface_text = font_default.render(size_text, True, (0, 180, 180))
-    screen.blit(surface_text, (size_input.x + 10, size_input.y + 5))
+        input_color = (120, 120, 120) if active else (90, 90, 90)
 
-    # Botón
-    pygame.draw.rect(screen, (0, 120, 200), size_button, border_radius=5)
-    button_text = font_default.render("OK", True, (255, 255, 255))
-    screen.blit(button_text, (size_button.x + 20, size_button.y + 5))
+        pygame.draw.rect(screen, input_color, size_input, border_radius=10)
+        surface_text = font_default.render(size_text, True, (0, 180, 180))
+        screen.blit(surface_text, (size_input.x + 10, size_input.y + 5))
 
-    # Texto dinámico
-    message = set_dimensiones(size_text)
-    message_surface = font_default.render(message, True, (0, 0, 0))
-    screen.blit(message_surface, (320, 280))
+        pygame.draw.rect(screen, (0, 120, 200), size_button, border_radius=5)
+        button_text = font_default.render("OK", True, (255, 255, 255))
+        screen.blit(button_text, (size_button.x + 20, size_button.y + 5))
 
-    pygame.display.flip()
+        set_dimensiones(size_text)
+
+    # Diseño del mapa
+    elif current_screen == "game":
+
+        dim_text = font_default.render(f"Dimensiones: {dim}x{dim}", True, (0,0,0))
+
+        dibujar_mapa(dim)
+
+        # TODO Boton de ayuda para explicar lo que hace cada pintura
+
+        screen.blit(dim_text, (40, 40))
+
+    pygame.display.flip() # Actualizar cambio de pantallas
 
 pygame.quit()
 sys.exit()
